a1b592909f80fbc599e2d097a49022a7bcfbe4a5
kang <bogyeongs2@daum.net>
2021-02-17 17:45:24 +0900

    Add

---
+++ README.md
@@ -0,0 +1,26 @@
+https://www.davepagurek.com/blog/using-swivel-in-batch-from-the-terminal/ 참고
+
+
+
+# simple
+1. python file을 Swivel 경로에 넣기
+
+![2](./img/2.png)
+
+2. swf 파일 경로 수정
+
+   ![2-1](./img/2-1.JPG)
+
+![3](./img/3.png)
+
+3. 프로그램 실행 시, mp4 파일로 자동 변환
+
+![4](./img/4.jpg)
+
+![4-1](./img/4-1.jpg)
+
+
+
+일일히 안해도됌ㅁㅁㅁㅁㅁㅁㅁㅁㅁ 하루만에 뚞딲뚞딲
+
+영상도 짤ㄴㅏ온당
---
+++ simple.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+import os
+import subprocess
+import time
+
+# swfPath, savePath, timeout
+swfpath = './2\\01\\swf\\'
+savepath = 'C:\\Users\\user\\Desktop\\workingfolder\\2\\01\\'
+timeout = 2000
+if not os.path.isdir(savepath):
+    os.mkdir(savepath)
+
+print("프로그램 실행")
+try:
+    # 폴더 파일 리스트 가져오기
+    filelist = [f for f in os.listdir(swfpath) if f.endswith('.swf')]
+
+    # 폴더 파일 리스트만큼 동영상 만들기
+    for l in filelist:
+        cmd = 'Swivel.exe ' + swfpath + l.rstrip('.swf') + '.swf -o '\
+                + savepath + l.rstrip('.swf') + '.mp4'
+        try:
+            print(l, "저장 중")
+            proc = subprocess.Popen(cmd, shell=True)
+            proc.communicate(timeout=timeout)
+        except subprocess.TimeoutExpired:
+            print(l + ", Timeout 되었습니다. 만약 인터셉트 부분이라면 swivel 프로그램을 종료 해주세요.")
+            subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=proc.pid))
+            time.sleep(5)
+            continue
+
+except Exception as e:
+    print("프로그램 실행 오류", e)
