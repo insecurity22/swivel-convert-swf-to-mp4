7ccfecdd4f8d1c2306e27b8d851346bc747d622f
insecurity22 <insecurity22@naver.com>
2021-03-02 10:38:34 +0900

    Update chapter.py

--- chapter.py
+++ chapter.py
@@ -46,7 +46,7 @@ try:
             if not count <= subchapter_index-1:
                 if l not in blacklist:
                     # Swivel 실행
-                    cmd = 'Swivel.exe ' + swfpath + l.rstrip('.swf') + '.swf -o ' + savepath_changed + l.rstrip('.swf') + '.mp4 -sm letterbox -s 1808x1166'
+                    cmd = 'Swivel.exe ' + swfpath + l.rstrip('.swf') + '.swf -o ' + savepath_changed + l.rstrip('.swf') + '.mp4 -s 1808x1166' # -sm letterbox
                     print(cmd)
                     try:
                         print(l, "저장 중")
