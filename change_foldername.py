bf2cf099fd2a9e3e77fc0ff18a45a213d04d63bb
insecurity22 <insecurity22@naver.com>
2021-03-13 01:44:37 +0900

    Update change_foldername.py

--- change_foldername.py
+++ change_foldername.py
@@ -1,62 +1,92 @@
 import os
 import sys
+import re
+
+# 그림2 -> video2
+# 그림-2 -> video_2
+
+path = "C:\\Users\\d0rk\\Desktop\\ex\\"
+oName = "그림"
+cName = "video"
+oMiddleText = ""
+cMiddleText = "_"
+oExtension = ".mp4"
+cExtension = ".mp3"
 
 # --- 인자값
-# 1. 변경하고 싶은 폴더 경로
-# 2. 넣고 싶은 문자
-# 3. 자를 문자
-# 4. 바꿀 문자
-# 5. 확장자
-path = "C:\\Users\\user\\Desktop\\ex\\"
-cName = "07"
-splitText = "-"
-changeSplitText = "_"
-extension = '.mp4'
-
-# ex) 07-003.mp4 -> 07_03.mp4
+# 1. 이름을 변경하고 싶은 폴더 경로
+# 2. 원본 문자         ex) 그림
+# 3. 변경할 문자       ex) video
+# 4. 원본 중간 문자     ex) video_2의 경우, _
+# 5. 변경할 중간 문자   ex) video-2로 변경하고 싶다면 -
+# 6. 원본 확장자    
+# 7. 변경할 확장자     
+# --> 원본과 변경할 확장자가 같을 경우, 같게 입력
 
 def changeName(path, cName):
     count = 0
     for filename in os.listdir(path):
-        print("\n---------------------------")
-        print("[변경 전]")
+
+        # 확장자가 다르면 변경할 파일에서 제외\
+        if re.findall("\.[a-zA-Z0-9]*", filename)[0] != oExtension:
+            continue
 
         try:
-            name = filename.split(splitText)[0]
-            index = filename.split(splitText)[1].replace(extension, "")
-            print("텍스트: " + name)
-            print("중간 기호:", splitText)
-            print("Index: " + index)
+            print("----------------------------")
+            print("[변경 전]")
             print("폴더 이름:", filename)
-            print("-------------------->")
 
-            # 앞에 0을 몇 개 붙일건지 ex) test_001, test_01
-            if int(index) < 10: changedIndex = "0" + str(int(index))
-            else: changedIndex = "" + str(int(index))
-
+            print("----------------------")
             print("[변경 후]")
-            print("텍스트: " + cName)
-            print("중간 기호: " + changeSplitText)
-            print("Index: " + changedIndex)
-            print("최종 변경 폴더 이름: " + cName + changeSplitText + changedIndex + extension)
-            print("---------------------------")
+
+            # 중간에 구분하는 문자가 없을 때. ex) 그림2
+            if oMiddleText == "":
+                index = re.findall("[\d*]\.", filename)[0].replace(".", "")
+                if cMiddleText == "":
+                    print("폴더 이름:", cName + index + cExtension)
+                else: # 구분할 문자 추가
+                    print("폴더 이름:", cName + cMiddleText + index + cExtension)
+
+            # 중간에 구분하는 문자가 있을 때. ex) 그림-2
+            else:
+                index = filename.split(oMiddleText)[1].replace(oExtension, "")
+                print("폴더 이름:", cName + cMiddleText + index + cExtension)
+            print("----------------------------")
+
+            # 만약 그림2 -> 그림02로 바꾸고 싶다면 index 앞에 "0"을 넣으면 된다.
+            # ex) print("폴더 이름:", cName + cMiddleText + "0" + index + cExtension)
 
             # 변경 확인
             if count == 0:
-                print("-- 최종 변경 폴더 이름을 확인해주세요.")
-                print("-- 전체 변경하시겠습니까? (Y|N)")
+                print("\n-- 바로 위, 변경 후 폴더 이름을 확인해주세요.")
+                print("-- 전체 변경하시겠습니까? (Y|N|E)")
+                print("-- 만약 프로그램을 종료하고 싶으시다면 E를 입력해주세요.")
                 answer = input()
-                if answer == "Y":
-                    count = 1
+                if answer == "Y": count = 1
                 elif answer == "N":
-                    sys.exit()
+                    continue
+                elif answer == "E": sys.exit()
             if count == 1:
                 try:
-                    os.rename(path + filename, path + cName + changeSplitText + changedIndex + extension)
-                    print("변경되었습니다.")
+                    if oMiddleText == "":
+                        if cMiddleText == "": os.rename(path+filename, path+cName+index+cExtension)
+                        else: os.rename(path+filename, path+cName+cMiddleText+index+cExtension)
+                        print("변경되었습니다.\n")
+                    else:
+                        os.rename(path+filename, path+cName+cMiddleText+index+cExtension)
                 except Exception as e:
-                    print("변경되지 않았습니다.")
-        except IndexError:
-            print("---> 나눌 문자열이 다릅니다")
+                    print("변경되지 않았습니다.\n")
 
-changeName(path, cName)
+        except Exception as e:
+            print("---> 변경할 수 없습니다.")
+            print("----------------------------\n")
+
+if __name__=="__main__":
+
+    print("\n---- 입력 값 확인 ----")
+    print("폴더 경로:", path)
+    print("원본 문자:", oName)
+    print("바꿀 문자:", cName)
+    print("---------------------\n")
+
+    changeName(path, cName)
